5d1d7dbe2c5f4bb929756da4fa23c4f95dcf1e41
kinouchi takahiro <drums.radwimps203@gmail.com>
2021-01-12 15:07:24 +0900

    solved NG message

--- s4/B203319/InformationEstimator.java
+++ s4/B203319/InformationEstimator.java
@@ -50,7 +50,7 @@ public class InformationEstimator implements InformationEstimatorInterface {
     public double estimation(){
         boolean [] partition = new boolean[myTarget.length+1];
         int np = 1<<(myTarget.length-1);
-        System.out.println("np="+np+" length="+myTarget.length);
+        //System.out.println("np="+np+" length="+myTarget.length);
         double value = Double.MAX_VALUE; // value = mininimum of each "value1".
 
         for(int p=0; p<np; p++) { // There are 2^(n-1) kinds of partitions.
